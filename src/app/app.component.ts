eb6eb6e81473a8afd4bfc21e6a7de13dc8ae13c5
bizexo <102514900+avktechhub@users.noreply.github.com>
2022-11-29 12:16:46 +0530

    biza-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'bizo';
   list:any;
+  profileNav:any;
 
   constructor(){
     this.list = [
-      { 'name': 'Home' },
+      { 'name': 'Home'
+       },
       { 'name': 'About',
       optionlist :[
           { 'name': 'vision' },
@@ -21,16 +23,29 @@ export class AppComponent {
         ],
         'option': true
      },
-      { 'name': 'Portfolio',
+      { 'name': 'Serivces',
+        'icon':true,
       optionlist :[
-          { 'name': 'Sevices' },
-          { 'name': 'Project' }
+          { 'name': 'School managment' },
+          { 'name': 'Employee Managment' }
         ],
         'option': true },
       { 'name': 'Contact' },
       { 'name': 'Setting' },
       { 'name': 'Sitmap'}
     ]
+
+    this.profileNav =[{ 
+      'name':'',
+      'image':true,
+      'imgURL':'https://scontent.fbom6-1.fna.fbcdn.net/v/t39.30808-1/277149398_5327575743941365_381370252198413949_n.jpg?stp=dst-jpg_p200x200&_nc_cat=106&ccb=1-7&_nc_sid=7206a8&_nc_ohc=04EKK0oUYNYAX-SWyrl&_nc_oc=AQnFxMMNwEcSwuFNnIC5b5JAjWxVTBoJsYxpOcAdmM_d12w7zSX5QaMuJZ8zvOGx-WQ&_nc_ht=scontent.fbom6-1.fna&oh=00_AfCq_yl0l1cGrvtcosMXz9crMRNyTXFRfXBj548SZC7Hkw&oe=638A67E7',
+    optionlist :[
+        { 'name': 'Edit profile' },
+        { 'name': 'Setting' },
+        { 'name': 'Logout' }
+      ],
+      'option': true
+   }]
   }
 
   ngOninit (){
--- src/componets/menu/menu.component.ts
+++ src/componets/menu/menu.component.ts
@@ -9,7 +9,7 @@ export class MenuComponent implements OnInit {
 
   @ViewChild('navContainer', { static: false }) nav!: ElementRef;
 
-  @Input() navConfig:Array<any>=[];
+  @Input() navConfig: Array<any> = [];
 
   constructor(private renderer: Renderer2) { }
 
@@ -17,31 +17,57 @@ export class MenuComponent implements OnInit {
   ngOnInit(): void {
   }
   ngAfterViewInit() {
+    
+    //Main nav bar
     const navlist = this.nav.nativeElement;
     let Ul = document.createElement('ul');
     this.renderer.appendChild(navlist, Ul);
-    
-    this.navConfig.filter((listname) => {return listname})
-    .map((ln)=> {
-      let li = document.createElement('li');
-      let a = document.createElement('a');
-      this.renderer.appendChild(Ul, li);
-      this.renderer.appendChild(li, a);
-      a.innerText = ln.name;
-          if(ln.option){
-              let optUl = document.createElement('ul');
-              li.appendChild(optUl);
-              
-              ln.optionlist.filter((optMenu:any) => {return optMenu})
-              .map((optMenulist:any)=> {
-                let optLi = document.createElement('li'),
-                optA = document.createElement('a');
-                optUl.appendChild(optLi)
-                optLi.appendChild(optA)
-                optA.innerText = optMenulist.name;
-              })
 
+    this.navConfig.filter((listname) => { return listname })
+      .map((ln) => {
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+        this.renderer.appendChild(Ul, li);
+        this.renderer.appendChild(li, a);
+        let aText = a.innerText = ln.name;
+        let disName = ln.name == undefined ? '' :  aText;
+
+         //Add icon here.
+        let addIcon = () => {
+          if(ln.image){
+            a.innerHTML = `<img src="${ln.imgURL}" alt="">`
           }
-    });
+          else{
+            a.innerHTML = 
+            `<svg class="bi bi-x-circle" fill="#ec5e45" height="16"
+                viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path
+                     d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+            </svg> <span>${disName}</span>
+            `
+          }
+         
+        };
+
+        //Condition for add icon or text.
+        ln.icon || ln.image ? addIcon() : disName;
+
+        //Dropdown code start here.
+        if (ln.option) {
+          let optUl = document.createElement('ul');
+          li.appendChild(optUl);
+
+          ln.optionlist.filter((optMenu: any) => { return optMenu })
+            .map((optMenulist: any) => {
+              let optLi = document.createElement('li'),
+                  optA = document.createElement('a');
+              optUl.appendChild(optLi)
+              optLi.appendChild(optA)
+              optA.innerText = optMenulist.name;
+            })
+        }
+      });
   }
 }

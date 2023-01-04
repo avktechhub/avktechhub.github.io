37ddb07ef8685c2d6b1af1a8598fd1a8787894df
avktechhub <avktechhub@outlook.com>
2023-01-04 05:38:38 +0000

    added authetication for login.

--- src/_helper/auth.interceptor.ts
+++ src/_helper/auth.interceptor.ts
@@ -1,18 +1,28 @@
 import { Injectable } from "@angular/core";
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { Observable } from "rxjs";
+import { TokenStorageService } from "src/_services/token-storage.service";
+
+
+const TOKEN_HEADER_KEY = 'Authorization';
 
 @Injectable({
    providedIn: 'root'
 })
 
 export class AuthInterceptor implements HttpInterceptor {
-    constructor(){
-       
-    }
+    constructor(private token: TokenStorageService){}
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
-        return next.handle(req); 
+        let authReq = req;
+        const token = this.token.getToken();
+        if(token != null){
+                authReq = req.clone({headers:req.headers.set(TOKEN_HEADER_KEY, 'Bearer' + token)})
+        }
+        return next.handle(authReq); 
     }
 }
 
-export const authInterceptorProviders = []
+export const authInterceptorProviders = [{
+    provide:HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi:true
+}]
